1478aebada4319e4b79725aa27fc0a3cd9b3b2c0
tome34 <wytome@163.com>
2018-12-15 15:30:51 +0800

    Initial commit

---
+++ app/src/main/java/com/fec/core/customview/MainActivity.kt
@@ -0,0 +1,16 @@
+package com.fec.core.customview
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import android.view.View
+import com.example.customview.widget.RatingBar
+
+class MainActivity : AppCompatActivity() {
+    private var mRatingBar: RatingBar? = null
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        mRatingBar = findViewById<View>(R.id.ratingbar) as RatingBar
+        mRatingBar?.setStar(3.0F)
+    }
+}
